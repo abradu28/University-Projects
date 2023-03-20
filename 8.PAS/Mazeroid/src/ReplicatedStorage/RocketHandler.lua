b01088ddb9447b924850fd6768a3c5bfb91f506a
abradu28 <79340394+abradu28@users.noreply.github.com>
2023-03-21 00:32:31 +0200

    Add rocket handler

---
+++ 8.PAS/Mazeroid/src/ReplicatedStorage/RocketHandler.lua
@@ -0,0 +1,115 @@
+local class = {};
+
+local step = game:GetService('RunService');
+
+function class:Run()
+	if self.Overwrite.CheckCollisions == nil then return warn("Can't run Rocket Class if Collision function is not overloaded"); end
+	if self.Overwrite.GetInputs == nil then return warn("Can't run Rocket Class if GetInputs function is not overloaded"); end
+	
+	self.RunningStatus = 1;
+	
+	self._trash.PhysicsHandler = step.Heartbeat:Connect(function(t)
+		if self.RunningStatus ~= 1 then return; end
+		
+		local count = math.round(t*60)
+		for i = 1,count do
+			t = 1/60;
+			local inputs = self.Overwrite.GetInputs();
+
+			self:UpdateVariables(inputs, t);
+			self:UpdateRocketPosition(inputs, t);
+			if self.Overwrite.CheckCollisions() == true then
+				if (self.Overwrite.OnCollision ~= nil) then
+					self.Overwrite.OnCollision();
+				end
+			end
+		end
+	end)
+end
+
+function class:Pause()
+	if self.RunningStatus == 1 then
+		self.RunningStatus = 2;
+	end
+end
+
+function class:Stop()
+	if self.RunningStatus == 1 or self.RunningStatus == 2 then
+		self.RunningStatus = 0;
+		self._trash.PhysicsHandler:Disconnect();
+		self._trash.PhysicsHandler = nil;
+	end
+end
+
+
+function class:SetInputsGetter(lambda)
+	self.Overwrite.GetInputs = lambda;
+	return self;
+end
+
+function class:SetCheckCollisions(lambda)
+	self.Overwrite.CheckCollisions = lambda;
+	return self;
+end
+
+function class:SetOnCollision(lambda)
+	self.Overwrite.OnCollision = lambda;
+	return self;
+end
+
+
+function class:GetSpeedAlpha()
+	return (self.CurrentSpeed - self.Settings.Speed.X)/(self.Settings.Speed.Y - self.Settings.Speed.X)
+end
+
+
+function class:UpdateRocketPosition(inputs, t)
+	local deltaAngle = math.rad(self.Settings.TurnSpeed) * t * inputs.X;
+	local deltaDist = self.CurrentSpeed * t;
+	self.Object:PivotTo(self.Object.PrimaryPart.CFrame*CFrame.new(0,0, -deltaDist)*CFrame.Angles(0, -deltaAngle, 0));
+end
+
+function class:UpdateVariables(inputs, t)
+	local deltaSpeed = self.Settings.Acceleration * t * inputs.Y;
+	self.CurrentSpeed = math.clamp(self.CurrentSpeed + (deltaSpeed), self.Settings.Speed.X, self.Settings.Speed.Y);
+end
+
+function class:new(sett)
+	sett = sett or {
+		Speed = Vector2.new(5, 50);	-- UDM/Secunda
+		Acceleration = 25;			-- UDM/Secunda
+		
+		TurnSpeed = 120;				-- Grade/Secunda
+		Radius = 1;
+	};
+	
+	local self = setmetatable({
+		Object = game.ReplicatedStorage.Rocket:Clone();
+		
+		RunningStatus = 0; 			-- 0=Stop 1=Running 2=Pause
+		CurrentSpeed = sett.Speed.X;
+		Settings = sett;
+		
+		Overwrite = {
+			CheckCollisions = nil;
+			OnCollision = nil;
+			
+			GetInputs = nil;
+		};
+		
+		_trash = {};
+	},{__index = class});
+	
+	self.Object.Parent = game.Workspace;
+	
+	return self;
+end
+
+function class:Destroy()
+	for i,v in pairs(self._trash) do
+		v:Disconenct();
+	end
+	self.Object:Destroy();
+end
+
+return class
--- 8.PAS/Mazeroid/src/StarterPlayer/StarterPlayerScripts/LocalScript.client.lua
+++ 8.PAS/Mazeroid/src/StarterPlayer/StarterPlayerScripts/LocalScript.client.lua
@@ -1,19 +1,14 @@
 local step = game:GetService('RunService');
 local uis = game:GetService('UserInputService');
 
-local rocket = game.Workspace.Rocket;
-local root = rocket.PrimaryPart;
-local meteor = script.Meteor;
+local meteor = game.ReplicatedStorage.Meteor;
 
+local plr = game.Players.LocalPlayer;
+local plrGui = plr:WaitForChild('PlayerGui');
 local cam = game.Workspace.CurrentCamera;
 cam.CameraType = Enum.CameraType.Scriptable;
 
-local cameraInterval = Vector2.new(15, 25);
-local speedInterval = Vector2.new(5, 50)
-local currentSpeed = speedInterval.X;
-local acceleration = 25;
-
-local turnAngle = 120;
+local rocketHandler = require(game.ReplicatedStorage.RocketHandler);
 
 local meteors = {};
 
@@ -21,22 +16,30 @@ local function Lerp(a, b, alpha)
 	return a + (b - a) * alpha;
 end
 
-local function UpdateVariables(t, moveVec)
-	local deltaSpeed = acceleration * t * moveVec.Y;
-	currentSpeed = math.clamp(currentSpeed + (deltaSpeed), speedInterval.X, speedInterval.Y);
-end
-
-local function UpdateRocketPosition(t, moveVec)
-	local deltaAngle = math.rad(turnAngle) * t * moveVec.X;
-	local deltaDist = currentSpeed * t;
-	rocket:PivotTo(root.CFrame*CFrame.new(0,0, -deltaDist)*CFrame.Angles(0, -deltaAngle, 0));
-end
-
-local function SetupGame()
+local function SetEffects()
+	game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false);
 	for i,v in pairs(game.Workspace.Layers:GetChildren()) do
 		v.Transparency = 1;
 	end
-	script.Skybox.Parent = game.Lighting;
+	game.ReplicatedStorage.Skybox.Parent = game.Lighting;
+
+	local spawnLocations = game.Workspace.MeteorSpawnLocations;
+	local start = spawnLocations.Origin.Position;
+	local r = (start - spawnLocations.End.Position).Magnitude;
+	for i = 1,1000 do
+		local newMeteor = meteor:Clone();
+		newMeteor.Parent = game.Workspace;
+		newMeteor.CFrame = CFrame.new(start-Vector3.new(0,40+math.random()*40,0))
+			*CFrame.Angles(0,math.random()*math.pi*2,0)
+			*CFrame.new(0,0,math.sqrt(math.random())*r)
+			*CFrame.Angles(math.random()*math.pi*2,math.random()*math.pi*2,math.random()*math.pi*2);
+		newMeteor.Size = Vector3.new(10,10,10)+Vector3.new(20,20,20)*math.random()
+		newMeteor.BrickColor = BrickColor.Blue();
+	end
+end
+
+local function SetupGame()
+	SetEffects();
 	
 	local spawnLocations = game.Workspace.MeteorSpawnLocations;
 	local start = spawnLocations.Origin.Position;
@@ -58,53 +61,48 @@ local function SetupGame()
 		};
 	end
 	
-	for i = 1,1000 do
-		local newMeteor = meteor:Clone();
-		newMeteor.Parent = game.Workspace;
-		newMeteor.CFrame = CFrame.new(start-Vector3.new(0,40+math.random()*40,0))
-			*CFrame.Angles(0,math.random()*math.pi*2,0)
-			*CFrame.new(0,0,math.sqrt(math.random())*r)
-			*CFrame.Angles(math.random()*math.pi*2,math.random()*math.pi*2,math.random()*math.pi*2);
-		newMeteor.Size = Vector3.new(10,10,10)+Vector3.new(20,20,20)*math.random()
-		newMeteor.BrickColor = BrickColor.Blue();
-	end
-end
+	local explosion = game.Workspace.Explosion;
+	explosion.Parent = nil;
+	local playerRocket = rocketHandler:new();
+	local cameraTarget = playerRocket;
+	playerRocket:SetInputsGetter(function()
+		local forward = uis:IsKeyDown(Enum.KeyCode.W);
+		local backward = uis:IsKeyDown(Enum.KeyCode.S);
+		local left = uis:IsKeyDown(Enum.KeyCode.A);
+		local right = uis:IsKeyDown(Enum.KeyCode.D);
 
-local function CheckCollisions()
-	local rocketPos = Vector2.new(root.Position.X, root.Position.Z);
-	local rocketR = root.Size.X/2;
-	
-	for i,v in pairs(meteors) do
-		local distVec = v.Position - rocketPos
-		local distSqr = distVec.X^2 + distVec.Y^2;
-		if (distSqr <= (rocketR + v.Radius)^2) then
-			return true, i;
+		return Vector2.new(((left == true) and -1 or 0) + ((right == true) and 1 or 0), ((forward == true) and 1 or 0) + ((backward == true) and -1 or 0));
+	end):SetCheckCollisions(function()
+		local root = playerRocket.Object.PrimaryPart;
+		local rocketPos = Vector2.new(root.Position.X, root.Position.Z);
+		local rocketR = playerRocket.Settings.Radius;
+
+		local s = os.clock();
+		for i,v in pairs(meteors) do
+			local distVec = v.Position - rocketPos
+			local distSqr = distVec.X^2 + distVec.Y^2;
+			if (distSqr <= (rocketR + v.Radius)^2) then
+				return true;
+			end
 		end
-	end
-	return false, -1;
+		return false;
+	end):SetOnCollision(function()
+		playerRocket:Stop();
+		explosion.Parent = game.Workspace;
+		explosion.Position = playerRocket.Object.PrimaryPart.Position;
+		explosion.ParticleEmitter:Emit(100);
+		playerRocket:Destroy();
+		cameraTarget = nil;
+	end):Run();
+	
+	local cameraInterval = Vector2.new(25, 50);
+	step.RenderStepped:Connect(function()
+		if cameraTarget == nil then return; end
+		
+		local zDist = Lerp(cameraInterval.X, cameraInterval.Y, cameraTarget:GetSpeedAlpha())
+		cam.CFrame = CFrame.new(cameraTarget.Object.PrimaryPart.Position)*CFrame.Angles(-math.pi/2,0,0)*CFrame.new(0,0,zDist)
+	end)
 end
 
-step.RenderStepped:Connect(function()
-	local zDist = Lerp(cameraInterval.X, cameraInterval.Y, (currentSpeed - speedInterval.X)/(speedInterval.Y - speedInterval.X))
-	cam.CFrame = CFrame.new(root.Position)*CFrame.Angles(-math.pi/2,0,0)*CFrame.new(0,0,zDist)
-end)
-
-step.Stepped:Connect(function(totalT, t)
-	local forward = uis:IsKeyDown(Enum.KeyCode.W);
-	local backward = uis:IsKeyDown(Enum.KeyCode.S);
-	local left = uis:IsKeyDown(Enum.KeyCode.A);
-	local right = uis:IsKeyDown(Enum.KeyCode.D);
-	
-	local moveVec = Vector2.new(((left == true) and -1 or 0) + ((right == true) and 1 or 0), ((forward == true) and 1 or 0) + ((backward == true) and -1 or 0));
-	
-	UpdateVariables(t, moveVec);
-	UpdateRocketPosition(t, moveVec);
-	local res, index = CheckCollisions();
-	if res == true then
-		warn("YES");
-		meteors[index].Model:Destroy();
-		table.remove(meteors, index);
-	end
-end)
 
 SetupGame();
